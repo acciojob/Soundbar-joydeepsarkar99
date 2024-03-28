8a43502ab2283e87622c04781aae23a80f630a96
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-03-28 15:11:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+let sounds // to hold all Audio 
+let audioHTML = document.querySelector('#my-audio');
+let btnList = document.querySelectorAll('.btn');
+
+for (let i = 0; i < btnList.length; i++) {
+	btnList[i].addEventListener("click", () => {
+		if (btnList[i].classList.contains('stop')) {
+			stopSounds();
+		} else {
+			playSound(i);
+		}
+	});
+}
+
+function playSound(i) {
+	let soundName = btnList[i].innerText;
+	audioHTML.setAttribute("src", `sounds/${soundName}.mp3`);
+	sounds = audioHTML 
+	audioHTML.play();
+}
+
+function stopSounds() {
+	sounds.pause();
+}
